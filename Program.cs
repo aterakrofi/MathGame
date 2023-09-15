84091e2cb4868a6a3501018f4d53743cd39fa3b7
Ater Akrofi <aterakrofi@gmail.com>
2023-09-14 23:55:14 -0500

    Planning how to code game

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Ater!");
+﻿internal class Program
+    {
+
+    /*
+ 1.You need to create a Math game containing the 4 basic operations
+ 2.The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100. Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.
+ 3. Users should be presented with a menu to choose an operation
+ 4.You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
+ 5. You don't need to record results on a database. Once the program is closed the results will be deleted.
+   
+
+        */
+
+    private static void Main(string [ ] args)
+        {
+        Console.WriteLine("Hello, World! Ater!");
+           //Menu  Addition , Subtraction , Multiplication , Division ,Previous game
+        //User can select operator using switch statements
+        //Prompt the user to give an answer?
+        
+        }
+    }
